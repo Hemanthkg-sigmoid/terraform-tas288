a524601a70914c324af242d9ba3ad348d61fd91e
hemanthkg-sigmoid <hemanth.kg@sigmoidanalytics.com>
2024-12-05 17:37:48 +0530

    initial commit

--- keypair.tf
+++ keypair.tf
@@ -2,11 +2,16 @@ resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
 }
 
-resource "aws_key_pair" "key_pair" {
-  key_name   = "dynamic-ssh-key"
-  public_key = tls_private_key.ssh_key.public_key_openssh
+# Generate a unique suffix for the key pair name
+resource "random_id" "key_pair_suffix" {
+  byte_length = 8
 }
 
+# Create the key pair with a unique name
+resource "aws_key_pair" "key_pair" {
+  key_name   = "dynamic-ssh-key-${random_id.key_pair_suffix.hex}"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
 output "private_key" {
   value     = tls_private_key.ssh_key.private_key_pem
   sensitive = true
